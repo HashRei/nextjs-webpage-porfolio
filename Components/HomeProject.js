ac494d408d0dd991fdb4f78ecdfa776d4b67d1df
HashRei <hashrei@protonmail.com>
2022-07-08 21:34:25 +0200

    Update shape of work elements on Homepage

--- Components/HomeProject.js
+++ Components/HomeProject.js
@@ -1,14 +1,14 @@
 import styles from '../styles/Home.module.css'
 import { FontAwesomeIcon } from "@fortawesome/react-fontawesome";
-import { faTelegramPlane } from "@fortawesome/free-brands-svg-icons"
+import { faGithub } from "@fortawesome/free-brands-svg-icons"
 import Link from 'next/link'
 import { HStack, Tag, TagLabel } from "@chakra-ui/react"
 
 const HomeProject = ({ currentTheme, project, id }) => {
     return (
-        <div className={styles.projectmain} style={{borderColor: currentTheme.accent, borderRadius: id % 2 === 0 ? '50px 0 0 0' : '0 0 50px 0'}}>
+        <div className={styles.projectmain} style={{borderColor: currentTheme.accent, borderRadius:  '25px'}}>
             <div className={styles.projectContent}>
-                <div className={styles.projectIcon} style={{color: currentTheme.accent, backgroundColor: currentTheme.footerColor, borderColor: currentTheme.accent}}><Link href={project.projectLink}><a><FontAwesomeIcon icon={faTelegramPlane} /></a></Link></div>
+                <div className={styles.projectIcon} style={{color: currentTheme.accent, backgroundColor: currentTheme.footerColor, borderColor: currentTheme.accent}}><Link href={project.githubLink}><a><FontAwesomeIcon icon={faGithub} /></a></Link></div>
                 <h1 className={styles.projectTitle}>{project.name}</h1>
                 <h2 className={styles.projectSubTitle} style={{color: currentTheme.subtext}}>{project.description}</h2>
                 <HStack spacing={2}>
