dd18bf261142ce07b92826ce5c5986fe59fe7773
HashRei <hashrei@protonmail.com>
2022-07-08 15:30:16 +0200

    Add tech labels to the project on the homepage

--- Components/HomeProject.js
+++ Components/HomeProject.js
@@ -2,6 +2,7 @@ import styles from '../styles/Home.module.css'
 import { FontAwesomeIcon } from "@fortawesome/react-fontawesome";
 import { faTelegramPlane } from "@fortawesome/free-brands-svg-icons"
 import Link from 'next/link'
+import { HStack, Tag, TagLabel } from "@chakra-ui/react"
 
 const HomeProject = ({ currentTheme, project, id }) => {
     return (
@@ -10,6 +11,19 @@ const HomeProject = ({ currentTheme, project, id }) => {
                 <div className={styles.projectIcon} style={{color: currentTheme.accent, backgroundColor: currentTheme.footerColor, borderColor: currentTheme.accent}}><Link href={project.projectLink}><a><FontAwesomeIcon icon={faTelegramPlane} /></a></Link></div>
                 <h1 className={styles.projectTitle}>{project.name}</h1>
                 <h2 className={styles.projectSubTitle} style={{color: currentTheme.subtext}}>{project.description}</h2>
+                <HStack spacing={2}>
+            {
+              project.tech.map((tech) => {
+                return (
+                  <div key={tech}>
+                    <Tag size="sm" borderRadius="md" variant="subtle" colorScheme={currentTheme.name === 'light' ? "cyan" : "purple"}>
+                      <TagLabel>{tech}</TagLabel>
+                    </Tag>
+                  </div>
+                )
+              })
+            }
+          </HStack>
                 <Link href='/work'><a className={styles.cta2} style={{ color: currentTheme.tertiary }}>View More</a></Link>
             </div>
         </div>
