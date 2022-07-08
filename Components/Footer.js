e893dfd9363baa27c129cd462e6da49cebe94a7e
HashRei <hashrei@protonmail.com>
2022-07-08 14:38:37 +0200

    Add socials icons to footer

--- Components/Footer.js
+++ Components/Footer.js
@@ -1,35 +1,59 @@
-import styles from '../styles/NavbarFooter.module.css';
-import { userinfo } from '../Constants/userinfo'
-import Link from 'next/link'
+import styles from "../styles/NavbarFooter.module.css";
+import { userinfo } from "../Constants/userinfo";
+import Link from "next/link";
+import { FontAwesomeIcon } from "@fortawesome/react-fontawesome";
 
 const Footer = ({ currentTheme }) => {
-    return (
-        <div className={styles.footermain} style={{ backgroundColor: currentTheme.footerColor, color: currentTheme.subtext }}>
-            <div className={styles.footertable}>
-                <Link href='/'><a><h2 className={styles.footerlogo}>{userinfo.logoText}</h2></a></Link>
-                <ul>
-                    <li className={styles.listHeading}>Socials</li>
-                    {userinfo.socials ?
-                        userinfo.socials.map((social, key) => {
-                            return (
-                                <Link href={social.link} key={key}><a><li>{social.type}</li></a></Link>
-                            )
-                        }) : null
-                    }
-                    <Link href={`mailto:${userinfo.contact.email ? userinfo.contact.email : ''}`}><a><li>Mail</li></a></Link>
-                </ul>
-                <ul>
-                    <li className={styles.listHeading}>Pages</li>
-                    <Link href='/'><a><li>Home</li></a></Link>
-                    <Link href='/#about'><a><li>About</li></a></Link>
-                    <Link href='/work'><a><li>Work</li></a></Link>
-                    <Link href='/contact'><a><li>Contact</li></a></Link>
-                </ul>
-            </div>
-            <hr style={{ height: '1px', backgroundColor: currentTheme.subtext, border: 'none', opacity: '0.5' }}></hr>
-            <h2 className={styles.footercontent}> <p>© {new Date().getFullYear()} {userinfo.logoText}</p></h2>
-        </div>
-    )
-}
+  const iconStyles = {
+    color: "#101010",
+  };
+  return (
+    <div
+      className={styles.footermain}
+      style={{
+        backgroundColor: currentTheme.footerColor,
+        color: currentTheme.subtext,
+      }}
+    >
+      <div className={styles.footertable}>
+        <Link href="/">
+          <a>
+            <h2 className={styles.footerlogo}>{userinfo.logoText}</h2>
+          </a>
+        </Link>
+        <ul className={styles.footertableSocials}>
+          {userinfo.socials
+            ? userinfo.socials.map((social, key) => {
+                return (
+                  <Link href={social.link} key={key}>
+                    <a>
+                      <li>
+                        <FontAwesomeIcon icon={social.icon} />
+                      </li>
+                      <li>{social.type}</li>
+                    </a>
+                  </Link>
+                );
+              })
+            : null}
+        </ul>
+      </div>
+      <hr
+        style={{
+          height: "1px",
+          backgroundColor: currentTheme.subtext,
+          border: "none",
+          opacity: "0.5",
+        }}
+      ></hr>
+      <h2 className={styles.footercontent}>
+        {" "}
+        <p>
+          © {new Date().getFullYear()} {userinfo.logoText}
+        </p>
+      </h2>
+    </div>
+  );
+};
 
-export default Footer
+export default Footer;
