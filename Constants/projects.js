6e766ecc72b5ce1e5e697b9a7745032ed9c12a43
HashRei <hashreig@protonmail.com>
2022-07-11 12:09:39 +0200

    Add new project pictures

--- Constants/projects.js
+++ Constants/projects.js
@@ -1,13 +1,11 @@
 // import project demo images as follows.
 import PROJECT1_1 from '../styles/projects/Project1_1.png';
 import PROJECT1_2 from '../styles/projects/Project1_2.png';
-import PROJECT1_3 from '../styles/projects/Project1_3.jpg';
-import PROJECT2_1 from '../styles/projects/Project2_1.jpg';
-import PROJECT2_2 from '../styles/projects/Project2_2.jpg';
-import PROJECT2_3 from '../styles/projects/Project2_3.jpg';
-import PROJECT3_1 from '../styles/projects/Project3_1.jpg';
-import PROJECT3_2 from '../styles/projects/Project3_2.jpg';
-import PROJECT3_3 from '../styles/projects/Project3_3.jpg';
+import PROJECT1_3 from '../styles/projects/Project1_3.png';
+import PROJECT2_1 from '../styles/projects/Project2_1.png';
+import PROJECT2_2 from '../styles/projects/Project2_2.png';
+import PROJECT3_1 from '../styles/projects/Project3_1.png';
+import PROJECT3_2 from '../styles/projects/Project3_2.png';
 
 // first 3 projects are visible on the homepage while all are visible on the work page.
 
@@ -30,15 +28,15 @@ export const projects = [
         projectLink: '/',
         videoLink: 'https://www.youtube.com/watch?v=0mvjyO3KZxk&t=2s', //deployed video link
         tech: ['React', 'GraphQL', 'Subgraph'],
-        photo: [PROJECT2_1, PROJECT2_2, PROJECT2_3],
+        photo: [PROJECT2_1, PROJECT2_2],
     },
     {
-        name: 'Project 3',
-        description: 'Lorem ipsum dolor sit amet consectetur adipisicing elit. Maxime mollitia, molestiae quas vel sint commodi repudiandae consequuntur voluptatum laborum',
-        githubLink: '/',
-        projectLink: '/',
+        name: 'On chain DAO ',
+        description: 'On chain DAO using TypeScript, Solidity, Hardhat for the dev side and Tally for the visual part',
+        githubLink: 'https://github.com/HashRei/on-chain-dao',
+        projectLink: 'https://www.tally.xyz/governance/eip155:4:0xBBFc55Fb69e7D940B3fA3B637c8E0C22704b681f',
         videoLink: '', //deployed video link
-        tech: ['Tech1', 'Tech2', 'Tech3'],
-        photo: [PROJECT3_1, PROJECT3_2, PROJECT3_3],
+        tech: ['Solidity', 'Hardhat', 'TypeScript'],
+        photo: [PROJECT3_1, PROJECT3_2],
     },
 ]
