b08a7466ab2ab7c01b17e437735ff45a88520c6b
HashRei <hashreig@protonmail.com>
2022-07-12 09:03:22 +0200

    Remove an emoji from a project description

--- Constants/projects.js
+++ Constants/projects.js
@@ -14,7 +14,7 @@ export const projects = [
     //leave the projectlink field blank if it the project has not been deployed, alerts will take care of it
     {
         name: 'NFT Marketplace', //project name
-        description: 'Ethereum based NFT marketplace using Polygon, Next.js, Tailwind CSS, and Hardhat', //project description
+        description: 'Ethereum based NFT marketplace using Polygon, Next.js, Tailwind CSS, and Hardhat.', //project description
         githubLink: 'https://github.com/HashRei/nft-marketplace', //github repo link
         projectLink: '', //deployed project link
         videoLink: '', //deployed video link
@@ -23,7 +23,7 @@ export const projects = [
     },
     {
         name: 'BAYC top holders subgraph',
-        description: 'Web application that returns Bored Ape Yacht Club NFT holders and the number of Bored Apes they hold🐱‍🏍',
+        description: 'Web application that returns Bored Ape Yacht Club NFT holders and the number of Bored Apes they hold.',
         githubLink: 'https://github.com/HashRei/bored-ape-yacht-club-top-holders-second-react-app',
         projectLink: '/',
         videoLink: 'https://www.youtube.com/watch?v=0mvjyO3KZxk&t=2s', //deployed video link
@@ -32,7 +32,7 @@ export const projects = [
     },
     {
         name: 'On chain DAO ',
-        description: 'On chain DAO using TypeScript, Solidity, Hardhat for the dev side and Tally for the visual part',
+        description: 'On chain DAO using TypeScript, Solidity, Hardhat for the dev side and Tally for the visual part.',
         githubLink: 'https://github.com/HashRei/on-chain-dao',
         projectLink: 'https://www.tally.xyz/governance/eip155:4:0xBBFc55Fb69e7D940B3fA3B637c8E0C22704b681f',
         videoLink: '', //deployed video link
